767823cc84e2181755e4cd68a0a4045eb283fc77
Yuku Kotani <yukukotani@gmail.com>
2021-03-27 19:43:13 +0900

    generate session service proro type

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p ./generated/services/user
+mkdir -p ./generated/services/session
 
 ### user
 yarn pbjs \
@@ -19,4 +20,23 @@ yarn pbts \
 sed -i -e "s/\[ 'Promise' \]\./Promise/g" "generated/services/user/index.d.ts"
 sed -i -e "s/\[ 'object' \]\.<string, any>/{ [k: string]: any }/g" "generated/services/user/index.d.ts"
 sed -i -e "s/\[ 'Array' \]\./Array/g" "generated/services/user/index.d.ts"
+###
+
+### session
+yarn pbjs \
+  --target static-module \
+  --no-encode \
+  --no-decode \
+  --path ../../ \
+  --out generated/services/session/index.js \
+  ./services/session-service/protos/SessionService.proto
+
+yarn pbts \
+  --out ./generated/services/session/index.d.ts \
+  ./generated/services/session/index.js
+
+### https://github.com/protobufjs/protobuf.js/issues/1222
+sed -i -e "s/\[ 'Promise' \]\./Promise/g" "generated/services/session/index.d.ts"
+sed -i -e "s/\[ 'object' \]\.<string, any>/{ [k: string]: any }/g" "generated/services/session/index.d.ts"
+sed -i -e "s/\[ 'Array' \]\./Array/g" "generated/services/session/index.d.ts"
 ###
