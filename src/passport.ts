3a2ec1b589cc1a2473bdd3a67ef249d1c6d2cd51
Yuku Kotani <yukukotani@gmail.com>
2021-03-27 20:49:36 +0900

    support twitter login

--- src/passport.ts
+++ src/passport.ts
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
+import { Strategy as TwitterStrategy } from 'passport-twitter';
 
 export function configurePassport() {
   passport.use(
@@ -9,6 +10,20 @@ export function configurePassport() {
         clientID: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         callbackURL: `${process.env.HOST_URL}/v1/auth/google/callback`,
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        cb(null, { id: profile.id });
+      },
+    ),
+  );
+
+  passport.use(
+    'twitter',
+    new TwitterStrategy(
+      {
+        consumerKey: process.env.TWITTER_CONSUMER_KEY!,
+        consumerSecret: process.env.TWITTER_CONSUMER_SECRET!,
+        callbackURL: `${process.env.HOST_URL}/v1/auth/twitter/callback`,
       },
       (accessToken, refreshToken, profile, cb) => {
         cb(null, { id: profile.id });
