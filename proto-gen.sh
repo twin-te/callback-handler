f7e48cf0a358bf80dea1dcc15756ca04544e1282
Yuku Kotani <yukukotani@gmail.com>
2021-03-27 16:58:11 +0900

    generate user service proto type

---
+++ proto-gen.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mkdir -p ./generated/services/user
+
+### user
+yarn pbjs \
+  --target static-module \
+  --no-encode \
+  --no-decode \
+  --path ../../ \
+  --out generated/services/user/index.js \
+  ./services/user-service/protos/UserService.proto
+
+yarn pbts \
+  --out ./generated/services/user/index.d.ts \
+  ./generated/services/user/index.js
+
+### https://github.com/protobufjs/protobuf.js/issues/1222
+sed -i -e "s/\[ 'Promise' \]\./Promise/g" "generated/services/user/index.d.ts"
+sed -i -e "s/\[ 'object' \]\.<string, any>/{ [k: string]: any }/g" "generated/services/user/index.d.ts"
+sed -i -e "s/\[ 'Array' \]\./Array/g" "generated/services/user/index.d.ts"
+###
